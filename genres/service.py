3c0ea94f1997a177ee123cc57807aeb86bb66710
heliont <heliomelont@gmail.com>
2025-04-01 02:29:59 -0300

    Fix: Setando cache local para listagem Genres,Movies,Reviews

--- genres/service.py
+++ genres/service.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from genres.repository import GenreRepository
 
 
@@ -7,10 +8,19 @@ class GenreService:
         self.genre_repository = GenreRepository()
 
     def get_genres(self):
-        return self.genre_repository.get_genres()
+        # Verifica se existe uma variavel em cache de sessão
+        if 'genres' in st.session_state:
+            return st.session_state.genres
+        # caso não tenha variavel em cache faz nova busca
+        genres = self.genre_repository.get_genres()
+        # Salva na sessão para evitar busca novamente na próxima vez que a página for carregada
+        st.session_state.genres = genres
+        return genres
 
     def create_genre(self, name):
         genre = dict(
             name=name,
         )
-        return self.genre_repository.create_genre(genre)
+        new_genre = self.genre_repository.create_genre(genre)
+        st.session_state.genres.append(new_genre)
+        return new_genre
--- movies/service.py
+++ movies/service.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from movies.repository import MovieRepository
 
 
@@ -7,7 +8,14 @@ class MovieService:
         self.movie_repository = MovieRepository()
 
     def get_movies(self):
-        return self.movie_repository.get_movies()
+        # Verifica se existe uma variavel em cache de sessão
+        if 'movies' in st.session_state:
+            return st.session_state.movies
+        # caso não tenha variavel em cache faz nova busca
+        movies = self.movie_repository.get_movies()
+        # Salva na sessão para evitar busca novamente na próxima vez que a página for carregada
+        st.session_state.movies = movies
+        return movies
 
     def create_movie(self, title, genre, release_date, actors, resume):
         movie = dict(
@@ -17,7 +25,9 @@ class MovieService:
             actors=actors,
             resume=resume,
         )
-        return self.movie_repository.create_movie(movie)
+        new_movie = self.movie_repository.create_movie(movie)
+        st.session_state.movies.append(new_movie)
+        return new_movie
 
     def get_movie_stats(self):
         return self.movie_repository.get_movie_stats()
